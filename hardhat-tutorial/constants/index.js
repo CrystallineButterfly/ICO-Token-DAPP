537697155d4ced214a928b3d938aa91a6027a0c1
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-06-16 16:14:55 +0100

    add all code for DAPP with ICO Token

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0x7c7B0Ffb08BFDEd2775EDc74ee383c82601f252b";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
